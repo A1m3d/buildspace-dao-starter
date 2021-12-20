319e52d86f34c064ef724b2b4de7e003baa12a2c
A1m3d <88129068+A1m3d@users.noreply.github.com>
2021-12-20 13:11:22 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -198,7 +198,7 @@ if (error && error.name === "UnsupportedChainIdError") {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to The-good-DAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
